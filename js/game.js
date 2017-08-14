564ee72be9adef89f7244485db3f01554671f085
Catherine Jean <catherinejeanbean@gmail.com>
2017-08-14 15:09:17 -0500

    EmptyFinder works, needs edge case for game over

--- js/game.js
+++ js/game.js
@@ -7,7 +7,8 @@
 // }
 
 var Game = function(stringOfSixteen = ""){
-	this.board = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
+	// this.board = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
+  this.board = [[3,3,3,3],[7,7,7,7],[5,6,7,5],[2,4,6,8]] 
   if (stringOfSixteen.length == 16) {
     var position = 0
     for ( row = 0; row < 4; row++) {
@@ -29,7 +30,7 @@ Game.prototype.generateRandomBoard = function() {
   //pick two different random positions and put a value of 2 in them
   var row = Math.floor(Math.random() * 4) //choose rand row and col since they are empty
   var col = Math.floor(Math.random() * 4)
-  this.board[row][col] = 2;
+  firstTwo = this.board[row][col] = 2;
   // check if 2 already exists before placing second 2
   do { 
     row = Math.floor(Math.random() * 4)
@@ -37,6 +38,19 @@ Game.prototype.generateRandomBoard = function() {
   } 
   while (this.board[row][col] == 2) 
   this.board[row][col] = 2;
+} 
+
+
+// method to find randome empty cell
+Game.prototype.emptyFinder = function() { //pick random cell that == 0
+  do {
+  var row = Math.floor(Math.random() * 4) //choose rand row and col 
+  var col = Math.floor(Math.random() * 4)
+  cellToCheck = this.board[row][col]
+  }
+  while (cellToCheck != 0) // is cellToCheck = 0?
+  return cellToCheck
+  console.log(cellToCheck)
 }
 
 
@@ -61,6 +75,7 @@ Game.prototype.toString = function() {
   return this.board[0].join("")+"\n"+this.board[1].join("")+"\n"+this.board[2].join("")+"\n"+this.board[3].join("")
 }
 
+// Move method for right , left , up , down
 Game.prototype.move = function(direction) {
   if (direction === "left") {
     this.left()
@@ -70,12 +85,14 @@ Game.prototype.move = function(direction) {
   this.htmlIfyer()
 }
   
+// Move left for a row, calls on rowleft 
 Game.prototype.left = function() {
   for (row = 0; row < 4; row++) {
     this.rowleft(this.board[row])
   }
 }
 
+// Make row go left
 Game.prototype.rowleft = function(rowvalues) {
   //move one row at a time
   var changed
@@ -92,7 +109,7 @@ Game.prototype.rowleft = function(rowvalues) {
       else if (rowvalues[i] !=0 && rowvalues[i] == rowvalues[i+1]) {
         rowvalues[i] += rowvalues[i+1]
         rowvalues[i + 1] = 0
-        console.log("merged left at i = " + i)
+        console.log("merged left at    i = " + i)
         changed = true
       }
     }
@@ -100,11 +117,22 @@ Game.prototype.rowleft = function(rowvalues) {
   while (changed)
 }
 
-Game.prototype.newNumber = function() {
-  //scan the whole board for at least one empty cell 
-  //choose random AND empty position
-  //choose a 2 or a 4 and stick in 
-}
+
+// Game.prototype.newNumber = function() {
+//   //scan the whole board for at least one empty cell
+//   while (this.board[row][col] == 0) 
+//   for (i = 0; i < 3; i++)
+//     do { 
+//       row = Math.floor(Math.random() * 4)
+//       col = Math.floor(Math.random() * 4)
+//     } 
+//   while (this.board[row][col] == !0) 
+//   this.board[row][col] = 2;
+//   //choose random AND empty position
+//   //choose a 2 or a 4 and stick in 
+// }
+
+
 
 //move every element as far as it will go 
 //try to combine elements if you can by adding and merging them into one cell
