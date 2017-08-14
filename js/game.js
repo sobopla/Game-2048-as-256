f7b45843634153a066be0b2c181daebc90f54bb6
Catherine Jean <catherinejeanbean@gmail.com>
2017-08-13 22:15:26 -0500

    Steps I forgot to commit on: Psuedocode is in txt file fixed js file to run locally without server taking out the leading single slash, add http to the js google 1. Created a game constructor 2. Created a game constructor function to create a random board as a separate function for the game constructor to call on 3. Starting on connecting the css

---
+++ js/game.js
@@ -0,0 +1,56 @@
+//two constructors 
+//one take no arguments
+//one takes a string
+// var Game = function() {
+//   this.board = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
+//   this.startTiles = 2;
+// }
+
+var Game = function(stringOfSixteen = ""){
+	this.board = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
+  if (stringOfSixteen.length == 16) {
+    var position = 0
+    for ( row = 0; row < 4; row++) {
+      for ( col = 0; col < 4; col++) {
+        this.board[row][col] = stringOfSixteen.substr(position, 1);
+        ++position;
+      }
+    }
+  }
+  // initialize a random board if no string is passed in 
+  else { 
+    this.generateRandomBoard()
+  }
+}
+
+// method to generate random board with 2 2
+Game.prototype.generateRandomBoard = function() {
+  //pick two different random positions and put a value of 2 in them
+  var row = Math.floor(Math.random() * 4) //choose rand row and col since they are empty
+  var col = Math.floor(Math.random() * 4)
+  this.board[row][col] = 2;
+  // check if 2 already exists before placing second 2
+  do { 
+    row = Math.floor(Math.random() * 4)
+    col = Math.floor(Math.random() * 4)
+  } 
+  while (this.board[row][col] == 2) 
+  this.board[row][col] = 2;
+}
+
+
+// method to fill html
+Game.prototype.htmlIfyer = function() {
+  
+}
+
+
+
+// update the board function
+// add things function
+// up move and functions check to add
+// down  move and functions check to add
+// right move and functions check to add
+//  left move and functions check to add
+// check bottom for adding?
+// 
